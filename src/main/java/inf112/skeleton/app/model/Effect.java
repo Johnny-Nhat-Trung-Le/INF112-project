2c651dfc1f215d853994c336669906cda3cd280d
Fredrik Rød <tilfredrik@icloud.com>
2024-02-20 11:56:36 +0100

    Effect added

---
+++ src/main/java/inf112/skeleton/app/model/Effect.java
@@ -0,0 +1,3 @@
+package inf112.skeleton.app.model;
+
+public record Effect(int duration, double speedBoost, double jumpBoost) {}
--- src/main/java/inf112/skeleton/app/model/Item.java
+++ src/main/java/inf112/skeleton/app/model/Item.java
@@ -1,5 +1,5 @@
 package inf112.skeleton.app.model;
 
-public abstract class Item {
-    
+public interface Item {
+    public Effect use();
 }
