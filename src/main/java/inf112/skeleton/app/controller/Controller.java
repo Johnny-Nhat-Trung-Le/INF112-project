8c4c3e664c98bd0b7f7e19f8f2dfecc4267011b4
Fredrik Rød <tilfredrik@icloud.com>
2024-02-21 12:22:20 +0100

    Controller changed to InputAdapter

--- src/main/java/inf112/skeleton/app/controller/Controller.java
+++ src/main/java/inf112/skeleton/app/controller/Controller.java
@@ -1,9 +1,10 @@
 package inf112.skeleton.app.controller;
 
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Input.Keys;
 
-public class Controller implements InputProcessor{
+public class Controller extends InputAdapter {
     private ControllableGameModel model;
     private ControllablePlayerModel playerModel;
     public Controller(ControllableGameModel model){
@@ -59,37 +60,5 @@ public class Controller implements InputProcessor{
     @Override
     public boolean keyTyped(char character) {
       return false;
-    }
-    /**
-     * Resten er vel irrelevant??
-     */
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchCancelled(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        return false;
-    }
-
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
-        return false;
-    }
-
-    @Override
-    public boolean scrolled(float amountX, float amountY) {
-        return false;
     }
 }
