38c7db39ee83dd67e37a558578845474c75ad92f
Fredrik Rød <tilfredrik@icloud.com>
2024-02-21 12:30:45 +0100

    Update EventHandler with javadoc

--- src/main/java/inf112/skeleton/app/event/EventHandler.java
+++ src/main/java/inf112/skeleton/app/event/EventHandler.java
@@ -1,6 +1,9 @@
 package inf112.skeleton.app.event;
 
 public interface EventHandler {
-    
+    /**
+     * Handles an {@link Event} posted in the {@link EventBus}
+     * @param event event posted in {@link EventBus}
+     */
     void handleEvent(Event event);
 }
