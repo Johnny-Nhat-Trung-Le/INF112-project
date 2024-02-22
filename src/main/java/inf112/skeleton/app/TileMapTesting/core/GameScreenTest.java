3af3aaefc6d60e2d8926386115a4693371574122
Johnny Le <johnnyle1410@gmail.com>
2024-02-22 12:36:11 +0100

    Objects in World Created

--- src/main/java/inf112/skeleton/app/TileMapTesting/core/GameScreenTest.java
+++ src/main/java/inf112/skeleton/app/TileMapTesting/core/GameScreenTest.java
@@ -6,10 +6,12 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
+import inf112.skeleton.app.TileMapTesting.helper.TileMapHelper;
 
 import static inf112.skeleton.app.TileMapTesting.helper.Constants.PPM;
 
@@ -19,11 +21,17 @@ public class GameScreenTest extends ScreenAdapter {
     private SpriteBatch batch;
     private World world;
     private Box2DDebugRenderer box2DDebugRenderer;
+
+    private OrthogonalTiledMapRenderer orthogonalTiledMapRenderer;
+    private TileMapHelper tileMapHelper;
     public GameScreenTest(OrthographicCamera camera) {
         this.camera = camera;
         this.batch = new SpriteBatch();
         this.world = new World(new Vector2(0,0), false);
         this.box2DDebugRenderer = new Box2DDebugRenderer();
+
+        this.tileMapHelper = new TileMapHelper(this);
+        this.orthogonalTiledMapRenderer = tileMapHelper.setupMap();
     }
 
     private void update() {
@@ -31,6 +39,7 @@ public class GameScreenTest extends ScreenAdapter {
         cameraUpdate(); // Camera to follow player
 
         batch.setProjectionMatrix(camera.combined);
+        orthogonalTiledMapRenderer.setView(camera);
 
         if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
             Gdx.app.exit();
@@ -49,10 +58,17 @@ public class GameScreenTest extends ScreenAdapter {
         Gdx.gl.glClearColor(0,0,0,0);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        // Render map before game objects
+        orthogonalTiledMapRenderer.render();
+
         // Between here you render the objects you will have in the game
         batch.begin();
 
         batch.end();
         box2DDebugRenderer.render(world, camera.combined.scl(PPM));
+    }
+
+    public World getWorld() {
+        return world;
     }
 }
---
+++ src/main/java/inf112/skeleton/app/TileMapTesting/helper/TileMapHelper.java
@@ -0,0 +1,69 @@
+package inf112.skeleton.app.TileMapTesting.helper;
+
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.objects.PolygonMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.Shape;
+import inf112.skeleton.app.TileMapTesting.core.GameScreenTest;
+
+import static inf112.skeleton.app.TileMapTesting.helper.Constants.PPM;
+
+public class TileMapHelper {
+
+    private TiledMap tiledMap;
+    private GameScreenTest gameScreenTest;
+
+    public TileMapHelper(GameScreenTest gameScreenTest) {
+        this.gameScreenTest = gameScreenTest;
+
+    }
+
+    public OrthogonalTiledMapRenderer setupMap() {
+        tiledMap = new TmxMapLoader().load("maps/map0.tmx");
+        parseMapObjects(tiledMap.getLayers().get("objects").getObjects());
+        return  new OrthogonalTiledMapRenderer(tiledMap);
+    }
+
+    private void parseMapObjects(MapObjects mapObjects) {
+        for(MapObject mapObject : mapObjects) {
+            // Create a static object
+            if (mapObject instanceof PolygonMapObject) {
+                createStaticBody((PolygonMapObject) mapObject);
+            }
+        }
+    }
+
+    /**
+     * Creating Static World Bodies
+     */
+    private void createStaticBody(PolygonMapObject polygonMapObject) {
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.StaticBody;
+        Body body = gameScreenTest.getWorld().createBody(bodyDef);
+        Shape shape = createPolygonShape(polygonMapObject);
+        body.createFixture(shape, 1000);
+        shape.dispose(); // No need the shape anymore
+    }
+
+
+    private Shape createPolygonShape(PolygonMapObject polygonMapObject) {
+        float[] vertices = polygonMapObject.getPolygon().getTransformedVertices();
+        Vector2[] worldVertices = new Vector2[vertices.length/2];
+
+        // always call the next one, so we always a tuple of the vertices
+        for(int i = 0; i < vertices.length / 2; i++) {
+            Vector2 current = new Vector2(vertices[i*2] / PPM, vertices[i * 2 + 1] / PPM);
+            worldVertices[i] = current;
+        }
+        PolygonShape shape = new PolygonShape();
+        shape.set(worldVertices);
+        return shape;
+    }
+}
