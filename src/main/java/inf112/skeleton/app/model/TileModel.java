d60c824dc985e44b6f9f1aecd58887a99e325701
Fredrik Rød <tilfredrik@icloud.com>
2024-02-22 13:44:16 +0100

    Implemented TileModel

--- src/main/java/inf112/skeleton/app/model/TileModel.java
+++ src/main/java/inf112/skeleton/app/model/TileModel.java
@@ -1,36 +1,72 @@
 package inf112.skeleton.app.model;
 
-import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.*;
 import inf112.skeleton.app.view.ViewableTile;
 
 public class TileModel implements ViewableTile, Physicable, Stepable {
+    private final World world;
+    private final Body body;
+    private float x;
+    private float y;
+    private float width;
+    private float height;
+
+    public TileModel(World world, float x, float y, float w, float h) {
+        this.world = world;
+        this.x = x;
+        this.y = y;
+        width = w;
+        height = h;
+        this.body = world.createBody(createBodyDef(x,y));
+        body.createFixture(createFixtureDef(w, h));
+    }
+
+    protected BodyDef createBodyDef(float x, float y) {
+        BodyDef def = new BodyDef();
+        def.type = BodyDef.BodyType.StaticBody;
+        def.position.set(x, y);
+        return def;
+    }
+
+    protected FixtureDef createFixtureDef(float w, float h) {
+        FixtureDef def = new FixtureDef();
+        def.density = 0.5f;
+        def.friction = 0.5f;
+        def.shape = createShape(w, h);
+        return def;
+    }
+
+    protected Shape createShape(float w, float h) {
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(w, h);
+        return shape;
+    }
+
     @Override
     public Body getBody() {
-        return null;
+        return body;
     }
 
     @Override
     public float getX() {
-        return 0;
+        return x;
     }
 
     @Override
     public float getY() {
-        return 0;
+        return y;
     }
 
     @Override
     public float getWidth() {
-        return 0;
+        return width;
     }
 
     @Override
     public float getHeight() {
-        return 0;
+        return height;
     }
 
     @Override
-    public void step() {
-
-    }
+    public void step() {}
 }
