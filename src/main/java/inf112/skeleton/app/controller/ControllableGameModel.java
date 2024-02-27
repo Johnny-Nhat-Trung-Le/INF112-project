2dab8a9c729def07d4afe2ba515ecbfb193e72d5
Fredrik Rød <tilfredrik@icloud.com>
2024-02-27 12:19:25 +0100

    ControllableGameModel fixed imports

--- src/main/java/inf112/skeleton/app/controller/ControllableGameModel.java
+++ src/main/java/inf112/skeleton/app/controller/ControllableGameModel.java
@@ -1,10 +1,6 @@
 package inf112.skeleton.app.controller;
 
-import inf112.skeleton.app.event.EventBus;
-import inf112.skeleton.app.grid.IGrid;
 import inf112.skeleton.app.model.*;
-
-import java.util.Iterator;
 
 public interface ControllableGameModel extends GameStateGettable, Stepable {
     /**
