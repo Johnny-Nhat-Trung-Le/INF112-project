4ebbaae7f387efe6164f682204121d436fb42422
Fredrik Rød <tilfredrik@icloud.com>
2024-03-19 12:11:21 +0100

    Hud added

--- src/main/java/inf112/skeleton/app/view/screen/GameScreen.java
+++ src/main/java/inf112/skeleton/app/view/screen/GameScreen.java
@@ -8,12 +8,14 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FillViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import inf112.skeleton.app.event.EventBus;
 import inf112.skeleton.app.model.event.EventDispose;
 import inf112.skeleton.app.model.event.EventStep;
+import inf112.skeleton.app.view.Hud;
 import inf112.skeleton.app.view.ViewableGameModel;
 import inf112.skeleton.app.view.ViewablePlayerModel;
 import inf112.skeleton.app.view.ViewableTile;
@@ -29,7 +31,9 @@ public class GameScreen implements Screen {
     private final OrthographicCamera gameCam;
     private final Viewport gamePort;
     private final SpriteBatch batch;
+    private final SpriteBatch batchHud;
     private final ITexturePack texturePack;
+    private final Stage hud;
 
     public GameScreen(ViewableGameModel model, EventBus bus, InputProcessor processor) {
         Gdx.graphics.setForegroundFPS(60);
@@ -41,11 +45,12 @@ public class GameScreen implements Screen {
         gameCam = new OrthographicCamera();
         gamePort = new FillViewport(VIEWPORT_WIDTH, VIEWPORT_HEIGHT, gameCam);
 
-        sRenderer = new ShapeRenderer();
-
         texturePack = new TexturePack();
 
+        sRenderer = new ShapeRenderer();
         batch = new SpriteBatch();
+        batchHud = new SpriteBatch();
+        hud = new Hud(batch);
     }
 
     @Override
@@ -63,6 +68,7 @@ public class GameScreen implements Screen {
 
         ScreenUtils.clear(0, 0, 0, 0);
 
+        gamePort.apply();
 
         sRenderer.begin(ShapeRenderer.ShapeType.Filled);
         renderBackground();
@@ -82,6 +88,9 @@ public class GameScreen implements Screen {
         );
 
         batch.end();
+
+        hud.getViewport().apply();
+        hud.draw();
     }
 
     private void updateCamToPlayer() {
@@ -89,8 +98,10 @@ public class GameScreen implements Screen {
         gameCam.position.set(p.getX() + p.getWidth() / 2, p.getY() + p.getHeight() / 2, 0);
         sRenderer.setProjectionMatrix(gameCam.combined);
         batch.setProjectionMatrix(gameCam.combined);
+        batchHud.setProjectionMatrix(gameCam.combined);
         gameCam.update();
     }
+
     private void renderBackground() {
         sRenderer.setColor(Color.BLACK);
         sRenderer.rect(-VIEWPORT_WIDTH, -VIEWPORT_HEIGHT, VIEWPORT_WIDTH * 3, VIEWPORT_HEIGHT * 3);
@@ -131,6 +142,7 @@ public class GameScreen implements Screen {
     @Override
     public void resize(int width, int height) {
         gamePort.update(width, height);
+        hud.getViewport().update(width, height);
     }
 
     @Override
