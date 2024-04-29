064e2a835df7d09a63f41e4d6588c259e4176b1f
Isak <isyau8632@uib.no>
2024-04-29 14:15:40 +0200

    added some more controllerTests

--- src/test/java/inf112/skeleton/app/controller/TestController.java
+++ src/test/java/inf112/skeleton/app/controller/TestController.java
@@ -1,14 +1,9 @@
 package inf112.skeleton.app.controller;
 
 import com.badlogic.gdx.Application;
-import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.backends.headless.HeadlessApplication;
-import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
-import com.badlogic.gdx.graphics.GL20;
 import inf112.skeleton.app.model.GameState;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -21,44 +16,6 @@ public class TestController {
     private ControllablePlayerModel mockPlayerModel;
     private Controller controller;
 
-    @BeforeAll
-    public static void beforeAll() {
-        HeadlessApplicationConfiguration config = new HeadlessApplicationConfiguration();
-        ApplicationListener listener = new ApplicationListener() {
-            @Override
-            public void create() {
-
-            }
-
-            @Override
-            public void resize(int width, int height) {
-
-            }
-
-            @Override
-            public void render() {
-
-            }
-
-            @Override
-            public void pause() {
-
-            }
-
-            @Override
-            public void resume() {
-
-            }
-
-            @Override
-            public void dispose() {
-
-            }
-        };
-        new HeadlessApplication(listener, config);
-        Gdx.gl20 = Mockito.mock(GL20.class);
-        Gdx.gl = Gdx.gl20;
-    }
 
     @BeforeEach
     public void setup() {
@@ -90,6 +47,41 @@ public class TestController {
         verify(mockPlayerModel, times(1)).moveUp(false);
     }
 
+    @Test
+    public void testKeyDownMoveDown() {
+        controller.keyDown(Input.Keys.S);
+        verify(mockPlayerModel, times(1)).moveDown(true);
+    }
+
+    @Test
+    public void testKeyUpMoveDown() {
+        controller.keyUp(Input.Keys.S);
+        verify(mockPlayerModel, times(1)).moveDown(false);
+    }
+
+    @Test
+    public void testKeyDownMoveLeft() {
+        controller.keyDown(Input.Keys.A);
+        verify(mockPlayerModel, times(1)).moveLeft(true);
+    }
+
+    @Test
+    public void testKeyUpMoveLeft() {
+        controller.keyUp(Input.Keys.A);
+        verify(mockPlayerModel, times(1)).moveLeft(false);
+    }
+
+    @Test
+    public void testKeyDownMoveRight() {
+        controller.keyDown(Input.Keys.D);
+        verify(mockPlayerModel, times(1)).moveRight(true);
+    }
+
+    @Test
+    public void testKeyUpMoveRight() {
+        controller.keyUp(Input.Keys.D);
+        verify(mockPlayerModel, times(1)).moveRight(false);
+    }
 
     @Test
     public void testKeyUpChangeStatePauseToActive() {
@@ -103,6 +95,36 @@ public class TestController {
         when(mockGameModel.getState()).thenReturn(GameState.ACTIVE);
         controller.keyUp(Input.Keys.P);
         verify(mockGameModel, times(1)).setState(GameState.PAUSE);
+    }
+
+    @Test
+    public void testKeyUpReset() {
+        when(mockGameModel.getState()).thenReturn(GameState.GAME_OVER);
+        controller.keyUp(Input.Keys.R);
+        verify(mockGameModel, times(1)).setState(GameState.MAIN_MENU);
+    }
+
+    @Test
+    public void testKeyUpHelpFromMain() {
+        when(mockGameModel.getState()).thenReturn(GameState.MAIN_MENU);
+        controller.keyUp(Input.Keys.H);
+        verify(mockGameModel, times(1)).setState(GameState.INFO);
+    }
+
+    @Test
+    public void testKeyUpHelpFromPause() {
+        when(mockGameModel.getState()).thenReturn(GameState.PAUSE);
+        controller.keyUp(Input.Keys.H);
+        verify(mockGameModel, times(1)).setState(GameState.INFO);
+        when(mockGameModel.getState()).thenReturn(GameState.INFO);
+
+    }
+
+    @Test
+    public void testKeyUpHelpToMain() {
+        when(mockGameModel.getState()).thenReturn(GameState.INFO);
+        controller.keyUp(Input.Keys.B);
+        verify(mockGameModel, times(1)).setState(GameState.MAIN_MENU);
     }
 
     @Test
